b2a291c1699085f42d207fbe17c76f93b78ab13c
Arthur Lima <arthur20036@gmail.com>
2024-05-13 16:05:07 -0300

    inicio

---
+++ src/main/java/com/api/v1/breakfast/Breakfast/BreakfastApplication.java
@@ -0,0 +1,13 @@
+package com.api.v1.breakfast.Breakfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakfastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/v1/breakfast/Breakfast/BreakfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.v1.breakfast.Breakfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakfastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
